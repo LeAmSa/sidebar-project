8c2e8c5176df1486147c2f9e68d8a551ed2e25b7
Leandro Amorim Salles <leandrosalles1878@gmail.com>
2022-07-11 15:04:22 -0300

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,11 +1,14 @@
 let btn = document.querySelector('#btn')
 let sidebar = document.querySelector('.sidebar')
 let btnSearch = document.querySelector('.btn-search')
+let homeContent = document.querySelector('.home_content')
 
 btn.onclick = function() {
     sidebar.classList.toggle('active')
+    homeContent.classList.toggle('active')
 }
 
 btnSearch.onclick = function() {
     sidebar.classList.toggle('active')
+    homeContent.classList.toggle('active')
 }
